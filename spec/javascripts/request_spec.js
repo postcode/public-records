6410d4a29a0c1e7b880cc067af8d0f3766fe10fd
Andy Hull <andyhull3@gmail.com>
2014-06-25 09:54:51 -0400

    changed source dir and source files to correctly pull in requests.js

--- spec/javascripts/request_spec.js
+++ spec/javascripts/request_spec.js
@@ -1,5 +1,9 @@
-describe("request", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
+describe("A Query model", function() {
+  it('should be able to create its application test objects',
+   function() {
+      var query = new Query();
+      expect(query).toBeDefined();
+      expect(MOCK_GET_DATA).toBeDefined();
+      expect(MOCK_POST_DATA).toBeDefined();
   });
 });
